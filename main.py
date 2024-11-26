918d526be5cae3fb587bd676f52647078489fe88
Said Torpoco <saideslander@gmail.com>
2024-11-25 22:41:09 -0500

    new funct

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-a = 1
-b = 2
-c = 3 
+from theme import InitSession
+
+a = InitSession(1)
+print(a)
+b = InitSession(a)
+print(b)
+c = InitSession(b)
+print(c)
 print("RAMA V1")
---
+++ theme.py
@@ -0,0 +1,3 @@
+def InitSession(a:int):
+    b = a + 1
+    return b
